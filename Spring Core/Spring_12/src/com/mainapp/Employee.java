1df6aa6f71a47f2beab3b94feaf8b605a434c79d
Dipesh <dipeshchauhan9898@gmail.com>
2026-08-25 00:53:11 +0530

    Learnt Properties Injection-Spring Core

---
+++ Spring Core/Spring_12/src/com/mainapp/Employee.java
@@ -0,0 +1,40 @@
+package com.mainapp;
+
+import java.util.Properties;
+
+public class Employee {
+	
+	private int eid;
+	private String ename;
+	private Properties properties;
+	/* private Map<String,Manager> mngr; */
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public Properties getProperties() {
+		return properties;
+	}
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", properties=" + properties + "]";
+	}
+	
+	
+
+	
+}
+
