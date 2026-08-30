ec3b765f590a4606e7ce9688a31495f296692d97
Dipesh <dipeshchauhan9898@gmail.com>
2026-08-30 11:55:32 +0530

    Implement Bean Life Cycle-PostConstruct,PreDestroy,Scope..using Annotation-Spring Core

--- Spring Core/Spring_14/src/com/mainapp/Employee.java
+++ Spring Core/Spring_14/src/com/mainapp/Employee.java
@@ -1,16 +1,48 @@
 package com.mainapp;
 
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+// @Component("emp") //With Name
+@Component //Without name
+@Lazy
+//@Scope("prototype")
 public class Employee {
 	
 	public Employee() {
 		System.out.println("Bean Instantiation");
 	}
 	
+	@PostConstruct
+	public void init() {
+		System.out.println("Bean initialization");
+	}
+	
 	public void test() {
 		System.out.println("Bean Tested");
 	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Bean Destroy");
+	}
+	
 }
 
+
+@Component("intern")
+@Lazy
+class Intern{
+	public Intern() {
+		System.out.println("Intern Bean Instatiation");
+	}
+	
+	public void test() {
+		System.out.println("Intern Tested");
+	}
+}
--- Spring Core/Spring_14/src/com/mainapp/Launch.java
+++ Spring Core/Spring_14/src/com/mainapp/Launch.java
@@ -8,8 +8,18 @@ public class Launch {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(MyConfiguration.class);
+		//Employee emp = (Employee) ac.getBean("emp"); //if name is defined
+		
+		Employee emp1 = ac.getBean(Employee.class);  //when bean name is not given
+		emp1.test();
+		System.out.println(emp1);
+		
+		Employee emp2 = ac.getBean(Employee.class);  //when bean name is not given
+		emp2.test();
+		System.out.println(emp2);
 		
 		
+		ac.close();
 	}
 
 }
